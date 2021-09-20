7eb5f942e574683b6c9dd90e7366eaec7e780c6d
Freem11 <freem1985@gmail.com>
2021-09-20 12:09:52 -0700

    refractored code

--- head.js
+++ head.js
@@ -1,20 +1,7 @@
+
 const head = function(actual) {
   return actual[0]
 }
 
-const assertEqual = function(actual, expected) {
-
-
-
-  if (actual === expected) {
-    console.log(`✅✅✅  Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
 
